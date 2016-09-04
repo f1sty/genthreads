00e509b7315b7e0107e1c46235ec11e044aeb9ef
Yurii Skrynnykov <truef1s7@gmail.com>
2016-09-04 21:25:28 +0300

    Add inbox_size to Actor constructor

--- genthreads/actor.py
+++ genthreads/actor.py
@@ -1,13 +1,21 @@
 from multiprocessing import Process
 
 
+class InboxFullError(Exception):
+    pass
+
+
 class Actor(Process):
-    def __init__(self):
+    def __init__(self, inbox_size=48):
         super(Actor, self).__init__()
         self._inbox = list()
+        self._inbox_size = inbox_size
 
     def send(self, value):
-        self._inbox.append(value)
+        if len(self._inbox) < self._inbox_size:
+            self._inbox.append(value)
+        else:
+            raise InboxFullError('no more space in inbox')
 
     @property
     def inbox(self):
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,15 @@
-from genthreads.actor import Actor
+import pytest
+from genthreads import actor
 
 
-def test_actor():
-    actor = Actor()
-    actor.send(42)
-    assert actor.inbox == [42]
+def test_actor_inbox():
+    act = actor.Actor()
+    act.send(42)
+    assert act.inbox == [42]
+
+
+@pytest.mark.xfail(raises=actor.InboxFullError)
+def test_full_inbox_error():
+    act = actor.Actor()
+    for i in range(49):
+        act.send(i)
